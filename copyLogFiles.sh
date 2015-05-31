0802c6a1b80332e79460ab2f050acec68fcbc571
Michael Reichmann <micha.reichmann@gmail.com>
2015-06-01 00:06:43 +0200

    added copylogfiles

---
+++ copyLogFiles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d $PWD/logs ]; then
+    mkdir -p $PWD/logs
+fi
+rsync -aP daq:~/sdvlp/keithleyClient/keithleyLog_2015_05_*.txt $PWD/logs
+
+if [ ! -d $PWD/eudaq_logs ]; then
+    mkdir -p $PWD/eudaq_logs
+fi
+rsync -aP rapidshare:/mnt/raid/psi_2015_05/logs_eudaq/*.log $PWD/eudaq_logs
